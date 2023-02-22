ec771c7aafc1590cf6bbf8c72e2dac350b581a81
thangb1910576 <122377655+thangb1910576@users.noreply.github.com>
2023-02-22 09:31:07 +0700

    Update index.js

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,7 @@
 const config = {
     app: {
-    port: process.env.PORT || 3000,
+        port : process.env.PORT || 3000,
     }
-    };
-    module.exports = config;
+};
+
+module.exports = config;
