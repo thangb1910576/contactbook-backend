d2828a8f7e7fa59aeaf0cc075c5a9bb568166e4d
thangb1910576 <122377655+thangb1910576@users.noreply.github.com>
2023-02-22 09:32:10 +0700

    Update contact.controller.js

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,24 +1,27 @@
 exports.create = (req, res) => {
-res.send({ message: "create handler" });
+    res.send({ message : "Create handler"});
 };
-exports.findALL = (req, res) => {
 
-res.send({ message: "findAll handler" });
+exports.findAll = (req, res) => {
+    res.send({ message : "findAll handler"});
 };
+
 exports.findOne = (req, res) => {
-
-res.send({ message: "findOne handler" });
+    res.send({ message : "findOne handler"});
 };
+
 exports.update = (req, res) => {
+    res.send({ message : "Update handler"});
 };
-res.send({ message: "update handler" });
-exports.delete = (req, res) => {
-};
-res.send({ message: "delete handler" });
-exports.deleteALL = (req, res) => {
-res.send({ message: "deleteAll handler" });
-};
-exports.findALLFavorite = (req, res) => {
 
-res.send({ message: "findAllFavorite handler" });
+exports.delete = (req, res) => {
+    res.send({ message : "delete handler"});
+};
+
+exports.deleteAll = (req, res) => {
+    res.send({ message : "deleteAll handler"});
+};
+
+exports.findAllFavorite = (req, res) => {
+    res.send({ message : "findAllFavorite handler"});
 };
